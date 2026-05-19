2849e37583855fb55a61cc520df63c42eaf88555
ai-prgmr <3.14.ai.prgmr@gmail.com>
2026-05-19 11:19:24 +0530

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/words-matter",
-  allowedDevOrigins: ['705c-49-249-2-114.ngrok-free.app'],
   output: "export",
+  reactStrictMode: true,
   images: {
     minimumCacheTTL: 31536000,
     unoptimized: true,
